270192375f1dd6ed27d4d4d4009d31646aca4c32
Giuseppe Mazzapica <giuseppe@zoom-multiservice.it>
2013-10-11 01:47:55 +0200

    Added Apigen Docs

---
+++ Doc/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","CleverRule"],["c","CleverRules"],["c","CleverRuleSanitize"],["f","cleverRulesInit()"],["f","get_the_clever_ruleid()"],["f","get_the_cleverlink()"],["f","register_clever_group()"],["f","register_clever_rule()"],["f","the_cleverlink()"]];
